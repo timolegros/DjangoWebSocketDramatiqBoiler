ababae46f3c267e067101833353c1dbc052873df
Timothee Legros <62490329+timolegros@users.noreply.github.com>
2020-09-16 22:36:07 +0200

    Update settings.py

--- BoilerPlateWebSocketDramatiq/settings.py
+++ BoilerPlateWebSocketDramatiq/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z0#rakmj#0b_1yhmvc$ei1ul&=y%84%*$no!*c@oo!x#56-z#n'
+SECRET_KEY = '######################################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
